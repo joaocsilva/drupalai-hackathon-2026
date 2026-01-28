927b3678958728bdcacdc256640ec6b28744af71
Piotr Smykaj <piotr.smykaj@gmail.com>
2026-01-28 12:18:57 +0100

    Update markup.

--- web/modules/custom/ai_elvis/js/index.js
+++ web/modules/custom/ai_elvis/js/index.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.ai_elvis = {
     attach: function (context) {
       const opened = `Collapse <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up h-4 w-4"><path d="m18 15-6-6-6 6"></path></svg>`;
-      const closed = `Expand <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down h-4 w-4"><path d="m6 9 6 6 6-6"></path></svg>`;
+      const closed = `Details <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down h-4 w-4"><path d="m6 9 6 6 6-6"></path></svg>`;
       let $buttons = $(once('root', '.collapse-button', context))
       if ($buttons.length) {
         $buttons.on('click', function () {
